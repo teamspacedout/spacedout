38770dff45287db89670d87a9c3a138849418916
Jokism <46876518+Jokism@users.noreply.github.com>
2022-10-30 07:55:44 -0400

    Adding functions for cloud-functions

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
 const express = require("express");
 const app = express();
 const cors = require("cors")({origin: true});
@@ -10,6 +12,41 @@ const cors = require("cors")({origin: true});
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+admin.initializeApp();
+
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+	// Grab the parameter
+	const original = req.query.text;
+
+	const writeResult = await admin.firestore().collection('messages').add({original: original});
+
+	const data = await admin.firestore().collection('messages').get().then(querySnapshot => {
+		let docs = querySnapshot.docs;
+		for (let doc of docs) {
+			const item = {
+				id: doc.id,
+				data: doc.data().original
+			};
+			console.log(item);
+		}
+	});
+	
+	const allMessagesRef = await admin.firestore().collection('messages').get();
+	
+	let messages = [];
+	const allMessages = allMessagesRef.docs;
+	for (let entry of allMessages) {
+		messages.push({id: entry.id, data: entry.data().original});
+	}
+	
+
+	res.json(messages.map(message => `ID: ${message.id} \tData: ${message.data}\n`));
+	
+	//res.json(allMessages.map(doc => doc))
+		
+	//res.json({result: `Message with ID: ${writeResult.id} and Data: ${original}`});
+});
 
 app.use(cors);
 
