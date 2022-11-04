7dd5807886cfb4baf738d48b57a0182bb08c87c6
diabeatz96 <75499553+diabeatz96@users.noreply.github.com>
2022-11-04 16:42:11 -0400

    Added Route Information and a bunch of new Pages

---
+++ src/routes/Search.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Search(props) {
+    return (
+        <div>This will be the search page (currently root)</div>
+    );
+}
+
+export default Search;
---
+++ src/routes/UserPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function UserPage(props) {
+    return (
+        <div>This is the user page</div>
+    );
+}
+
+export default UserPage;
