2d8c9ff2999f939abb122cf5e161e179c6bf6519
diabeatz96 <75499553+diabeatz96@users.noreply.github.com>
2022-11-04 17:00:59 -0400

    Added several routes necessary for page development.

--- src/App.js
+++ src/App.js
@@ -5,12 +5,40 @@ import Root from "./routes/Root";
 import {Background} from "./components/Background";
 import UserLogin from "./lib/context";
 import {useUserAuth} from "./lib/hooks";
+import Login from "./routes/Login";
+import SignUp from "./routes/SignUp";
+import PlanetPage from "./routes/PlanetPage";
+import UserPage from "./routes/UserPage";
+import Freeroam from "./routes/Freeroam";
 
 
 const router = createBrowserRouter([{
     path: "/",
     element: <Root />,
-}])
+}, {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/signup",
+        element: <SignUp />,
+    },
+    {
+        path: "/:user/:planet",
+        element: <PlanetPage />,
+    },
+    {
+        path: "/:user",
+        element: <UserPage />,
+    },
+    {
+        path: "/freeroam",
+        element: <Freeroam />,
+    },
+    {
+        path: "/",
+        element: <Root />,
+    },])
 
 
 function App() {
