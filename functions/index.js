b8db26abc515859d5e5b5efa2f3ca5e9fe00385f
Jokism <46876518+Jokism@users.noreply.github.com>
2022-11-28 23:54:24 -0500

    Created DB User PUT endpoint implementation.

--- functions/index.js
+++ functions/index.js
@@ -491,7 +491,60 @@ app.get("/api/db/user/:username", (req, res) => {
  * }
  * @return Map - An object containing the updated User document data
  */
-app.put("/api/db/user/:username", (req, res) => {});
+app.put("/api/db/user/:username", (req, res) => {
+  const username = req.params.username.trim();
+  const usersRef = firestore.collection("Users");
+  const userDoc = usersRef.where("Username", "==", username).limit(1);
+
+  const updatedUser = {
+    Profile_data: req.body.profileData ? req.body.profileData : undefined,
+    Profile_settings: req.body.profileSettings ? req.body.profileSettings : undefined,
+  };
+
+  // Check if data is undefined
+  let isDataEmpty = true;
+
+  for (const key in updatedUser) {
+    if (updatedUser[key] !== undefined && Object.values(updatedUser[key]).length > 0) {
+      isDataEmpty = false;
+    } else {
+      delete updatedUser[key];
+    }
+  }
+
+  if (!isDataEmpty) {
+    return userDoc.get().then((userDocument) => {
+      if (userDocument.docs.length > 0) {
+        const docId = userDocument.docs[0].id;
+        const userProfileData = userDocument.docs[0].data().Profile_data;
+        const userProfileSettings = userDocument.docs[0].data().Profile_settings;
+        const uid = userDocument.docs[0].data().uid;
+
+        // Update User document
+        return usersRef.doc(docId).update(updatedUser).then((writeResult) => {
+          const originalData = {
+            Profile_data: userProfileData,
+            Profile_settings: userProfileSettings,
+          };
+          const logUserUpdate = {
+            UpdatedAt: writeResult.writeTime.toDate(),
+            Username: username,
+            uid: uid,
+            originalData,
+            updatedFields: updatedUser,
+          };
+          return res.status(200).send(logUserUpdate);
+        });
+      } else {
+        return res.status(500).send({Error: "User not found"});
+      }
+    }).catch((error) => {
+      return res.status(500).send({Error: error.code});
+    });
+  } else {
+    return res.status(400).send({Error: "No valid data was sent"});
+  }
+});
 
 
 /** Firestore Planets Subcollection Endpoints
