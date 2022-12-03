4e653dc1e83e0979018354caa480a0107f3ece0e
diabeatz96 <75499553+diabeatz96@users.noreply.github.com>
2022-12-03 12:49:55 -0500

    Changed several facets of the User Page and tailwind.config.js

--- src/routes/UserPage.js
+++ src/routes/UserPage.js
@@ -42,11 +42,97 @@ function UserPage() {
     }
 
 
+    if(!page.data) {
+        return (
+           <main>
+               <div className="h-screen flex justify-center items-center">
+                   <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full "
+                        role="status">
+                       <span className="visually-hidden"></span>
+                   </div>
+               </div>
+           </main>
+        )
+    }
+
     return (
         <main>
             <NavBar/>
-            <div className=" grid grid-cols-1 md:grid-cols-3 gap-10 h-screen w-screen">
-                <PlanetOrbit
+            <div className=" grid grid-rows-3 md:grid-cols-1 gap-3 h-screen w-screen">
+
+                <div className="grid grid-cols-3">
+                <h1 className=" pt-10 col-start-2 col-end-3 text-4xl text-center font-bold tracking-tight sm:text-center sm:text-6xl ">
+                    Welcome to {page.data.Username}'s Profile.
+                </h1>
+                </div>
+
+                <div className="bg-blue-600 bg-opacity-30 row-start-2 row-end-4 grid grid-cols-3 ">
+                        <div className="col-span-2">
+
+                            <div className="card bg-primary bg-opacity-70 text-primary-content m-10 grid grid-cols-2">
+                                <div className="card-body">
+
+                                    <h2 style={{fontSize: "4em"}} className="card-title">
+                                        <div className="avatar online">
+                                            <div className="w-24 rounded-full">
+                                                <img src="https://t4.ftcdn.net/jpg/00/77/46/11/360_F_77461112_WFAEbGQnlfZDUnJ984ncl44ItgtYw0gJ.jpg"/>
+                                            </div>
+                                        </div>
+                                        {page.data.Username}
+                                    </h2>
+                                    <div className="btn-group ">
+                                        <button className="btn "> Add Friend </button>
+                                        <button className="btn">  Message </button>
+                                        <button className="btn">  Explore Planets </button>
+                                    </div>
+                                    <h1 className="sm:text-4xl capitalize font-space italic underline">MISSION LOG</h1>
+                                    <p>{page.data.Profile_data.Bio}</p>
+                                    <div className="card-actions justify-end">
+                                    </div>
+                                </div>
+                                <div className="grid place-content-center">
+                                    <div className="stats stats-vertical shadow bg-purple-500 text-white scale-125">
+
+                                        <div className="stat">
+                                            <div className="stat-title">Planet Count</div>
+                                            <div className="stat-value">{page.data.Planet_count}</div>
+                                            <div className="stat-desc"></div>
+                                        </div>
+
+                                        <div className="stat">
+                                            <div className="stat-title">Friend Count</div>
+                                            <div className="stat-value">{page.data.Friend_count}</div>
+                                            <div className="stat-desc"></div>
+                                        </div>
+
+                                        <div className="stat">
+                                            <div className="stat-title">Account Created</div>
+                                            <div className="stat-value">Some Date</div>
+                                            <div className="stat-desc"></div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    {
+
+                    }
+                        <div className="grid grid-rows-1 place-content-center pb-3">
+                            <div>
+                                <Planet scale={"300%"}/>
+                            </div>
+                        </div>
+                </div>
+
+            </div>
+        </main>
+    );
+}
+/*
+       <PlanetOrbit
                     scale={"200%"}
                     defaultPlanet= {<Planet scale={"70%"} img = "/assests/planets/Lava/4.png"/>}
                     remainPlanets = {
@@ -59,15 +145,15 @@ function UserPage() {
                         <PlanetButton scale = {0.75} btnProperties = "btn-circle bg-pink-500 grid place-items-center" reactIcon = {<FaMagic className="text-white"/>}/>,
                         <PlanetButton scale = {0.75} btnProperties = "btn-circle bg-gray-500 grid place-items-center" reactIcon = {<FaGoogle className="text-white"/>}/>,
                     ]}/>
-                <PlanetOrbit
-                    defaultPlanet= {<Planet img = "/assests/planets/Ice/4.png"/>}
-                    remainPlanets = {[<PlanetButton scale = {3} />, <PlanetButton/>]}/>
-                <PlanetOrbit  showOrbit={true} remainPlanets = {[<PlanetButton/>, <PlanetButton/>, <PlanetButton/>, <PlanetButton/>, <PlanetButton/>, <PlanetButton/>, <PlanetButton/>,<PlanetButton/>, <PlanetButton/>]}/>
-            </div>
-        </main>
-    );
-}
 
+                <div className="text-white place-self-center">
+                    <div> Username: {page.data.Username} </div>
+                    <div> User ID:  {page.data.uid} </div>
+                    <div> Friend Count: {page.data.Friend_count} </div>
+                    <div> Planet Count: {page.data.Planet_count} </div>
+                    <div> {page.data.Profile_data.Bio}</div>
+                </div>
+ */
 
 
 export default UserPage;
